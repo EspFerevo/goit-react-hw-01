980a226beb818e715c9164b368ab2bb39b237985
EspFerevo <olekchekrenov@gmail.com>
2024-06-08 11:06:55 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Profile from "./components/Profile/Profile";
 import userData from "./userData.json";
 import FriendList from "./components/FriendList/FriendList";
-import friends from "./friends.json";
+import friends from "./friend.json";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import transactions from "./transactions.json";
 import "./App.css";
